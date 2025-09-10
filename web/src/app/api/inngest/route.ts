a462f06aa8190da03b0eaffaba276b8369100f06
AnatolKir <88631659+AnatolKir@users.noreply.github.com>
2025-09-10 14:51:37 -0400

    fix: move all app content to src/app for Vercel deployment
    - Vercel root is set to 'web' directory
    - All app content must be within web/src/app
    - Fix import paths for inngest (now 4 levels up)
    - This should restore the site functionality

--- web/src/app/api/inngest/route.ts
+++ web/src/app/api/inngest/route.ts
@@ -1,6 +1,6 @@
 import { serve } from 'inngest/next';
-import { inngest } from '../../../inngest/client';
-import { allJobs } from '../../../inngest';
+import { inngest } from '../../../../inngest/client';
+import { allJobs } from '../../../../inngest';
 
 // Create the Inngest HTTP handler
 export const { GET, POST, PUT } = serve({
